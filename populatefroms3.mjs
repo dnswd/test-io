68075bcfe943e633bebf88ed5a7f4ea0a0fd6f5e
Dennis Al Baihaqi Walangadi <dennis.walangadi@gmail.com>
2023-06-07 13:03:26 +0700

    bandwidth testing

---
+++ populatefroms3.mjs
@@ -0,0 +1,107 @@
+import { Storage } from "@google-cloud/storage";
+import crypto from 'crypto';
+import path from 'path'
+import fs from 'fs'
+
+const generateRandomKiloBytes = (kiloByteSize) => {
+  const byteSize = kiloByteSize * 1024;
+  return crypto.randomBytes(byteSize);
+};
+
+const logger = (message) => {
+  console.log(`${Date.now()/1000},${message}`)
+}
+
+async function wait(s) {
+  return new Promise(resolve => setTimeout(resolve, s*1000));
+}
+
+const serviceKey = path.join(process.cwd(), "keys.json");
+
+const storage = new Storage({
+  keyFilename: serviceKey,
+  projectId: "rclone-tasla",
+});
+
+const bucket = storage.bucket("test-centralized-store");
+
+const kbs = [4, 16, 64, 1*1024, 4*1024, 16*1024];
+
+// const uploaded = {}
+
+// function uploadGCP(unit) {
+//   return new Promise((resolve, reject) => {
+//     const filename = `test1-${unit}KB`
+//     const blobStream = bucket
+//       .file(filename)
+//       .createWriteStream({ resumable: false });
+//     blobStream.on("finish", () => {
+//       logger(`Finished upload ${unit} kilobytes`)
+//       resolve(filename)
+//     }).on("error", (e) => {
+//       logger(`Unable to upload ${unit} kilobytes: \n${e}`)
+//       reject(e)
+//     });
+//     logger(`Uploading ${unit} kilobytes`)
+//     blobStream.end(generateRandomKiloBytes(unit))
+//     // return filename
+//   })
+// }
+
+async function downloadGCP(filename) {
+  const file = bucket.file(filename)
+  const readStream = file.createReadStream();
+  const writeStream = fs.createWriteStream(filename);
+
+  readStream.pipe(writeStream);
+  return new Promise((resolve, reject) => {
+    writeStream.on('finish', resolve);
+    writeStream.on('error', reject);
+  });
+}
+
+async function cleanup(filename) {
+  // await bucket.file(filename).delete()
+  await fs.unlink(filename, (err) => {
+    if (err) {
+      logger(err)
+    }
+  })
+}
+
+async function main() {
+  // logger("Uploading files")
+
+  // for (const unit of kbs) {
+  //   const filename = await uploadGCP(unit)
+  //   uploaded[unit] = filename
+  //   logger("Waiting for ambient traffic")
+  //   await wait(5)
+  // }
+
+  // logger("Mark download traffic, waiting for 10 seconds")
+  //   await wait(10)
+
+  const sizeKb = 4
+  const filename = `test-${sizeKb}KB`
+  logger(`Downloading file ${filename}`)
+  await downloadGCP(filename)
+  logger("Cleaning local and GCS")
+  await cleanup(filename)
+
+  // logger("Downloading files")
+  // for (const unit in uploaded) {
+  //   const filename = uploaded[unit];
+  //   await downloadGCP(filename)
+  //   logger("Waiting for ambient traffic")
+  //   await wait(5)
+  // }
+
+  // logger("Cleaning local and GCS")
+  // for (const unit in uploaded) {
+  //   const filename = uploaded[unit];
+  //   await cleanup(filename)
+  // }
+}
+
+main().catch(e => logger(e))
--- setupipfs.sh
+++ setupipfs.sh
@@ -1,8 +1,19 @@
-sudo su
 wget https://dist.ipfs.tech/kubo/v0.20.0/kubo_v0.20.0_linux-amd64.tar.gz
 tar -xvf kubo_v0.20.0_linux-amd64.tar.gz
 chmod +x ./kubo/install.sh
 ./kubo/install.sh
 exit
+
 ipfs --version
 ipfs init
+ipfs bootstrap rm --all
+ipfs bootstrap add /ip4/10.184.0.7/tcp/4001/ipfs/12D3KooWLM9A3HNnCxXHJCknnjBE3iaqyhtS6iLRH4fFgVMVBxzG
+
+LIBP2P_FORCE_PNET=1 ipfs daemon
+
+scp swarm.key halcyon@34.101.231.188:~/.ipfs/
+scp swarm.key halcyon@34.101.87.56:~/.ipfs/
+
+scp -r testcases halcyon@34.101.87.56:~
+
+QmZLws5v55MQJmyTo2deZfp3Z1KuKa9MwyCaymTxqXXmkh
